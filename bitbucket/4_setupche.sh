ba3e96938725f71d677b11b32d87139663cd6acb
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-01-13 11:35:36 +0200

    fixup! Simplify setu[
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- bitbucket/4_setupche.sh
+++ bitbucket/4_setupche.sh
@@ -9,4 +9,4 @@ PRIVATE_KEY=$(cat ./certs/privatepkcs8.pem | sed 's/-----BEGIN PRIVATE KEY-----/
 oc patch checluster/eclipse-che --patch "{\"spec\":{\"server\":{\"customCheProperties\": {\"CHE_OAUTH1_BITBUCKET_PRIVATEKEY\": \"$PRIVATE_KEY\"}}}}" --type=merge -n $NS
 BITBUCKET_HOST=$(oc get routes -n bitbucket -o json | jq -r '.items[0].spec.host')
 oc patch checluster/eclipse-che --patch "{\"spec\":{\"server\":{\"customCheProperties\": {\"CHE_OAUTH1_BITBUCKET_ENDPOINT\": \"https://$BITBUCKET_HOST\"}}}}" --type=merge -n $NS
-oc patch checluster/eclipse-che --patch "{\"spec\":{\"server\":{\"customCheProperties\": {\"CHE_BITBUCKET_SERVER_ENDPOINTS\": \"https://$BITBUCKET_HOST\"}}}}" --type=merge -n $NS
+oc patch checluster/eclipse-che --patch "{\"spec\":{\"server\":{\"customCheProperties\": {\"CHE_INTEGRATION_BITBUCKET_SERVER__ENDPOINTS\": \"https://$BITBUCKET_HOST\"}}}}" --type=merge -n $NS
