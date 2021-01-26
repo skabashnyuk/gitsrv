f1ef82b69e807f01127dd199a4289d7d90d8e0f7
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2021-01-26 17:44:10 +0200

    Update 2_generate_keys.sh

--- bitbucket/2_generate_keys.sh
+++ bitbucket/2_generate_keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -rf ./certs
 mkdir certs
-openssl genrsa -out ./certs/private.pem 1024
+openssl genrsa -out ./certs/private.pem 2048
 openssl rsa -in ./certs/private.pem -pubout > ./certs/public.pub
 openssl pkcs8 -topk8 -inform pem -outform pem -nocrypt -in ./certs/private.pem -out ./certs/privatepkcs8.pem 
 openssl rand -base64 24 > ./certs/bitbucket_server_consumer_key
