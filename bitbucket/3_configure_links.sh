df7d0f25729d1a1fe28e39decd0c5dc32965ded4
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-02-02 11:58:39 +0200

    Fix links

--- bitbucket/3_configure_links.sh
+++ bitbucket/3_configure_links.sh
@@ -5,12 +5,13 @@ oc project $NS
 CHE_ROUTE=$(oc get route/che --namespace=$NS -o=jsonpath={'.spec.host'})
 BITBUCKET_ROUTE=$(oc get route/bitbucket --namespace=bitbucket -o=jsonpath={'.spec.host'})
 CHE_SERVER_URL='https://'${CHE_ROUTE}
+BITBUCKET_URL='https://'${BITBUCKET_ROUTE}
 PUB_KEY=$(cat ./certs/public.pub | sed 's/-----BEGIN PUBLIC KEY-----//g' |  sed 's/-----END PUBLIC KEY-----//g' | tr -d '\n')
 CONSUMER_KEY=$(cat ./certs/bitbucket_server_consumer_key)
 SHARED_SECRET=$(cat ./certs/bitbucket_shared_secret)
 echo '     '
 echo '     '
-echo ' Open https://'$BITBUCKET_ROUTE
+echo ' Open '$BITBUCKET_URL
 echo '     '
 echo '     '
 echo ' Go to Administration -> Application Links'
@@ -21,9 +22,9 @@ echo ' Application Type:      Generic Application'
 echo ' Service Provider Name: Che'
 echo ' Consumer key:          '$CONSUMER_KEY
 echo ' Shared secret:         '$SHARED_SECRET
-echo ' Request Token URL:     '$CHE_SERVER_URL'/plugins/servlet/oauth/request-token'
-echo ' Access token URL:      '$CHE_SERVER_URL'/plugins/servlet/oauth/access-token'
-echo ' Authorize URL:         '$CHE_SERVER_URL'/plugins/servlet/oauth/authorize'
+echo ' Request Token URL:     '$BITBUCKET_URL'/plugins/servlet/oauth/request-token'
+echo ' Access token URL:      '$BITBUCKET_URL'/plugins/servlet/oauth/access-token'
+echo ' Authorize URL:         '$BITBUCKET_URL'/plugins/servlet/oauth/authorize'
 echo ' Create incoming link:  true'
 echo '    '
 echo ' Next screen   '
